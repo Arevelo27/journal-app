22bfac9dfe73d6db8ee6946448da03df2e70eca5
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-21 15:57:44 -0500

    RegisterPage - Diseño

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,55 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid2, Link, TextField, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material'
+import { AuthLayout } from '../layout/AuthLayout'
+
 export const RegisterPage = () => {
   return (
-    <div>Registerpage</div>
+    <AuthLayout title="Crear Cuenta">
+      <form>
+        <Grid2 container>
+          <Grid2 item size={{ xs: 12 }} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre Completo"
+              type="text"
+              placeholder="Nombre Completo"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 item size={{ xs: 12 }} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="Correo"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid2>
+
+          <Grid2 container spacing={2} size={{ xs: 12 }} sx={{ mb: 2, mt: 1 }}>
+            <Grid2 size={{ xs: 12, sm: 6 }} >
+              <Button variant="contained" fullWidth>
+                Crear Cuenta
+              </Button>
+            </Grid2>
+          </Grid2>
+
+          <Grid2 container direction="row" justifyContent="flex-end" size={{ xs: 12 }}>
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color='inherit' to="/auth/login">
+              <Typography>Ingresar</Typography>
+            </Link>
+          </Grid2>
+
+        </Grid2>
+      </form>
+    </AuthLayout>
   )
 }
