24e20f2f9d7059a8db1c8448c5faa955f81b458d
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-21 15:53:27 -0500

    AuthLayout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,67 +1,53 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid2, Link, TextField, Typography } from '@mui/material'
 import { Google } from '@mui/icons-material'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const LoginPage = () => {
   return (
-    <Grid2
-      container
-      spacing={0} // Corregido aquí
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid2 item
-        className='box-shadow'
-        size={{ xs: 12, sm: 6 }} // Ajustado el tamaño
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-      >
-        <Typography variant="h5" sx={{ mb: 1 }}>Login</Typography>
-
-        <form>
-          <Grid2 container>
-            <Grid2 item size={{ xs: 12 }} sx={{ mt: 2 }}>
-              <TextField
-                label="Correo"
-                type="email"
-                placeholder="Correo"
-                fullWidth
-              />
-            </Grid2>
-            <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
-              <TextField
-                label="Contraseña"
-                type="password"
-                placeholder="Contraseña"
-                fullWidth
-              />
-            </Grid2>
-
-            <Grid2 container spacing={2} size={{ xs: 12 }} sx={{ mb: 2, mt: 1 }}>
-              <Grid2 size={{ xs: 12, sm: 6 }} >
-                <Button variant="contained" fullWidth>
-                  Login
-                </Button>
-              </Grid2>
-              <Grid2 size={{ xs: 12, sm: 6 }}>
-                <Button variant="contained" fullWidth>
-                  <Google />
-                  <Typography sx={{ ml: 1 }}>Google</Typography>
-                </Button>
-              </Grid2>
-            </Grid2>
-
-            <Grid2 container direction="row" justifyContent="flex-end" size={{ xs: 12 }}>
-              <Typography sx={{ mr: 1 }}>¿No tienes cuenta?</Typography>
-              <Link component={RouterLink} color='inherit' to="/auth/register">
-                <Typography>Crear Cuenta</Typography>
-              </Link>
-            </Grid2>
-
+    <AuthLayout title="Login">
+      <form>
+        <Grid2 container>
+          <Grid2 item size={{ xs: 12 }} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="Correo"
+              fullWidth
+            />
           </Grid2>
-        </form>
-      </Grid2>
-    </Grid2>
+          <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid2>
+
+          <Grid2 container spacing={2} size={{ xs: 12 }} sx={{ mb: 2, mt: 1 }}>
+            <Grid2 size={{ xs: 12, sm: 6 }} >
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid2>
+            <Grid2 size={{ xs: 12, sm: 6 }}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid2>
+          </Grid2>
+
+          <Grid2 container direction="row" justifyContent="flex-end" size={{ xs: 12 }}>
+            <Typography sx={{ mr: 1 }}>¿No tienes cuenta?</Typography>
+            <Link component={RouterLink} color='inherit' to="/auth/register">
+              <Typography>Crear Cuenta</Typography>
+            </Link>
+          </Grid2>
+
+        </Grid2>
+      </form>
+    </AuthLayout>
   )
 }
