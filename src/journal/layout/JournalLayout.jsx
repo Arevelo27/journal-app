9583bebbf717c6ed0a7c6355bc73a6ec02785f84
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-21 16:47:41 -0500

    NavBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material"
+import { Navbar } from "../../ui";
 
 const drawerWidth = 240;
-
 
 export const JournalLayout = ({ children }) => {
     return (
         <Box sx={{ display: 'flex' }}>
             {/* Nabvar */}
+            <Navbar drawerWidth={ drawerWidth } />
 
             {/* Sidebar */}
 
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,5 +1,33 @@
-export const Navbar = () => {
+import { AppBar, Grid2, IconButton, Toolbar, Typography } from '@mui/material'
+import { LogoutOutlined, MenuOutlined } from '@mui/icons-material'
+
+export const Navbar = ({ drawerWidth = 240 }) => {
     return (
-        <></>
+        <AppBar
+            position='fixed'
+            sx={{
+                width: { sm: `calc(100% - ${ drawerWidth }px)` }, // Ajustado el tamaño en pantalla
+                ml: { sm: `${drawerWidth}px` }
+            }}
+        >
+            <Toolbar>
+                <IconButton
+                    color='inherit'
+                    edge='start'
+                    sx={{ mr: 2, display: { sm: 'none' } }}
+                >
+                    <MenuOutlined />
+                </IconButton>
+
+                <Grid2 container alignItems='center' justifyContent='space-between' sx={{ flexGrow: 1 }}>
+                    <Typography variant='h6' noWrap component='div'>Journal App</Typography>
+
+                    <IconButton color='error'>
+                        <LogoutOutlined />
+                    </IconButton>
+
+                </Grid2>
+            </Toolbar>
+        </AppBar>
     )
 }
