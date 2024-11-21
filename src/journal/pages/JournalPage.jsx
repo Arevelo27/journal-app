6376bf53d0ac47fca09cc849b5a0d4ecdce127cd
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-21 16:05:58 -0500

    JournalLayout y JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,10 @@
 import { Typography } from '@mui/material'
-import { MailOutline } from '@mui/icons-material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      <MailOutline  />
-    </>
+    <JournalLayout>
+      <Typography>Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran. Muchas veces, contribuyen a detener la acción y preparar el escenario de los hechos que siguen.</Typography>
+    </JournalLayout>
   )
 }
