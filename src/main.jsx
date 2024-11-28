6836fb5d2763462e59f15d9f957f8055d9439a84
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-27 23:29:55 -0500

    Configurando Redux en nuestra aplicación

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 import { JournalApp } from './JournalApp'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
