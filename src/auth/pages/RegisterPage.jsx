afdb92f9f1aea3324cab4ecd4422e73595b94b9e
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2024-11-28 23:46:17 -0500

    Formulario de registro de usuarios

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,26 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid2, Link, TextField, Typography } from '@mui/material'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hook';
 
 export const RegisterPage = () => {
+
+  const formData = {
+    email: 'andres@gmail.com',
+    password: '123456',
+    displayName: 'Andres Castillo'
+  }
+
+  const { displayName, email, password, onInputChange, formState } = useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState)
+  }
+
   return (
     <AuthLayout title="Crear Cuenta">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid2 container>
           <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
             <TextField
@@ -13,6 +28,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Nombre Completo"
               fullWidth
+              name='displayName'
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid2>
           <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
@@ -21,6 +39,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="Correo"
               fullWidth
+              name='email'
+              value={email}
+              onChange={onInputChange}
             />
           </Grid2>
           <Grid2 size={{ xs: 12 }} sx={{ mt: 2 }}>
@@ -29,12 +50,19 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name='password'
+              value={password}
+              onChange={onInputChange}
             />
           </Grid2>
 
           <Grid2 container spacing={2} size={{ xs: 12 }} sx={{ mb: 2, mt: 1 }}>
-            <Grid2 size={{ xs: 12, sm: 6 }} >
-              <Button variant="contained" fullWidth>
+            <Grid2 size={{ xs: 12 }} >
+              <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+              >
                 Crear Cuenta
               </Button>
             </Grid2>
