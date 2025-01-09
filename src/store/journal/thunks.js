c9dfe1c33a4e094163eb9fc3545e0ad83255598c
Andres Felipe Castillo Revelo <andrcarv@suramericana.com.co>
2025-01-08 22:13:23 -0500

    Crear una nueva nota en firebase con thunks

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,13 +1,21 @@
+import { doc, collection, setDoc } from "firebase/firestore/lite"
+import { FirebaseDB } from "../../firebase/config"
+
 export const startNewNote = () => {
     return async (dispatch, getState) => {
+
+        const { uid } = getState().auth
 
         const newNote = {
             title: '',
             body: '',
             date: new Date().getTime(),
             imageUrls: [],
-            uid: getState().auth.uid
         }
+
+        const newDoc = await doc(collection(FirebaseDB, `${uid}/journal/notes`))
+        await setDoc(newDoc, newNote)
+
         // dispatch
         // dispatch(newNote);
         // dispatch(activarNote(newNote));
